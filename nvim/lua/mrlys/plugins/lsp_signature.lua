36448abfe6e7aa8f957cf5e0efc0f5e0a4649184
Thomas <lys@pop-os.localdomain>
2023-09-18 19:29:04 +0200

    add keymap for signature help

--- nvim/lua/mrlys/plugins/lsp_signature.lua
+++ nvim/lua/mrlys/plugins/lsp_signature.lua
@@ -2,5 +2,8 @@ return {
   "ray-x/lsp_signature.nvim",
   event = "VeryLazy",
   opts = {},
-  config = function(_, opts) require'lsp_signature'.setup(opts) end
+  config = function(_, opts) require'lsp_signature'.setup(opts) end,
+    keys = {
+        {'<C-p>', "<cmd>lua require('lsp_signature').toggle_float_win()<cr>", desc = "Toggle Signature help"}
+    }
 }
