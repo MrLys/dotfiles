0411ee9bf90b49c41b466dca87e3408dd020fc18
Thomas <lys@pop-os.localdomain>
2023-09-18 19:24:06 +0200

    Add more plugins

--- nvim/lua/mrlys/plugins/harpoon.lua
+++ nvim/lua/mrlys/plugins/harpoon.lua
@@ -1,16 +1,16 @@
 return  {
      'theprimeagen/harpoon',
-     config = function() 
-	     local mark = require("harpoon.mark")
-	     local ui = require("harpoon.ui")
-
-
-	     vim.keymap.set("n", "<leader>.", mark.add_file)
-	     vim.keymap.set("n", "<leader>;", ui.toggle_quick_menu)
-
-	     vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)  
-	     vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-	     vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-	     vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-     end
+    lazy = false,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = true,
+    keys = {
+        {"<leader>.", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon"},
+        {"<leader>;", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks"},
+        {"<C-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Navigate to first marked file"},
+        {"<C-t>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Navigate to second marked file"},
+        {"<C-n>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Navigate to third marked file"},
+        {"<C-s>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Navigate to forth marked file"},
+    }
 }
--- nvim/lua/mrlys/plugins/lsp-zero.lua
+++ nvim/lua/mrlys/plugins/lsp-zero.lua
@@ -18,7 +18,7 @@ return {
 		{'L3MON4D3/LuaSnip'},
 		{'rafamadriz/friendly-snippets'},
 	},
-	config = function() 
+	config = function()
 		local lsp = require("lsp-zero")
 		require("lsp_lines").setup()
 
@@ -52,14 +52,14 @@ return {
 			local opts = { buffer = bufnr, remap = false }
 
 			vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-			vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-			vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-			vim.keymap.set("n", "<leader>vd", function() require("lsp_lines").toggle() end, opts)
-			vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-			vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-			vim.keymap.set("n", "<leader>l", function() vim.lsp.buf.code_action() end, opts)
-			vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-			vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {desc = 'Show workspace symbols', buffer = bufnr, remap = false})
+            vim.keymap.set("n", "<leader>vd", function() require("lsp_lines").toggle() end, {desc = 'Toggle lsp lines', buffer = bufnr, remap = false})
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set("n", "<leader>l", function() vim.lsp.buf.code_action() end, {desc = 'Show code action', buffer = bufnr, remap = false})
+            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, {desc = 'Show usage', buffer = bufnr, remap = false})
+            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, {desc = 'Rename', buffer = bufnr, remap = false})
 			vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 		end)
 
