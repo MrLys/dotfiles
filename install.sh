a7ceb9253e9d65227fb539a9f066959de271306f
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2024-06-05 13:30:59 +0200

    fix install script and remove old tmux conf stuff

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
-mv ~/.config/nvim ~/.config/nvim.bak
-mkdir -p ~/.config/nvim
-cp -r lazyvim/* ~/.config/nvim
-
-mkdir -p ~/.local/scripts
-cp tmux-sessionizer ~/.local/scripts
+mv $HOME/.config/nvim $HOME/.config/nvim.bak
+mkdir -p $HOME/.config/nvim
+cp -r lazyvim/* $HOME/.config/nvim
+#tmux
+sudo apt install tmux
+# tmux plugin manager
+TPMDIRECTORY="$HOME/.tmux/plugins/tpm"
+if [ ! -d "$TPMDIRECTORY" ]; then
+	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+	echo "TPM installed"
+else
+	echo "TPM already installed"
+fi
+# tmux fzf
+cp tmux.conf $HOME/.tmux.conf
+tmux run-shell "source ~/.tmux.conf"
+# tmux sessionizer
+mkdir -p $HOME/.local/scripts
+cp tmux-sessionizer $HOME/.local/scripts
